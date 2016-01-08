307a2fc35f460807223519333676157422de4d0c
Dhaivat Pandit <me@dhaiv.at>
2016-01-08 14:48:12 -0800

    Feature: added timeout for graceful shutdown
        * dexter will try to kill monitored targets within reasonable time
        * once time is up os.Exit(1) will be called.

--- dexter_test.go
+++ dexter_test.go
@@ -2,6 +2,7 @@ package dexter
 
 import (
 	"os"
+	"os/signal"
 	"syscall"
 	"testing"
 	"time"
@@ -17,8 +18,8 @@ func (d dcloser) Close() error {
 func TestDexter(t *testing.T) {
 
 	stage1 := NewTarget("stage1")
+	stage1.Add(1)
 	f1 := func(target *Target, in <-chan string) {
-		target.Add(1)
 		defer target.Done()
 		for _ = range in {
 		}
@@ -27,8 +28,8 @@ func TestDexter(t *testing.T) {
 	stage1.TrackChannel(c1In)
 
 	stage2 := NewTarget("stage2")
+	stage2.Add(1)
 	f2 := func(target *Target, in <-chan int) {
-		target.Add(1)
 		defer target.Done()
 		for _ = range in {
 			_ = <-in
@@ -38,8 +39,8 @@ func TestDexter(t *testing.T) {
 	stage2.TrackChannel(c2In)
 
 	stage3 := NewTarget("stage3")
+	stage3.Add(1)
 	f3 := func(target *Target, in <-chan bool) {
-		target.Add(1)
 		defer target.Done()
 		for _ = range in {
 			<-in
@@ -64,4 +65,52 @@ func TestDexter(t *testing.T) {
 		syscall.Kill(os.Getpid(), syscall.SIGINT)
 	}()
 	dex.WaitAndKill()
+}
+
+func TestForceKillInterval(t *testing.T) {
+
+	stage1Stuck := NewTarget("stage_stuck")
+	stage1Stuck.Add(1)
+	f1 := func(target *Target) {
+		for {
+			time.Sleep(1 * time.Second)
+		}
+	}
+	go f1(stage1Stuck)
+
+	pass := make(chan bool)
+	fail := make(chan bool)
+	dummyExitFunc := func(code int) {
+		if code == 0 {
+			fail <- true
+		} else {
+			pass <- true
+		}
+	}
+
+	dex := &Dexter{
+		waiter:          make(chan os.Signal),
+		targets:         []*Target{},
+		forceKillWindow: 1 * time.Second,
+		exitFunc:        dummyExitFunc,
+	}
+	signal.Notify(dex.waiter, syscall.SIGINT, syscall.SIGTERM)
+	dex.Track(stage1Stuck)
+
+	go func() {
+		// kill after it waiting for small amount of time
+		time.Sleep(10 * time.Millisecond)
+		syscall.Kill(os.Getpid(), syscall.SIGINT)
+	}()
+
+	go dex.WaitAndKill()
+	// this is proxy dummyExitFunc above working
+	select {
+	case <-pass:
+		// good - it worked
+	case <-fail:
+		// got unexpected code in return
+		t.Fail()
+	}
+
 }
